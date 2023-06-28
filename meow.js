fdbe0f33e7bf7a80f0526236c3253be5818df9ec
sidharth tripathi <sidharthtripathi807@gmail.com>
2023-06-29 05:07:04 +0530

    create server for catgpt

---
+++ meow.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const cors = require('cors')
+const server = express();
+const url = 'localhost'
+const port = 3000
+server.use(cors())
+server.get('/resp',(req,res)=>{
+let data = randomMeow();
+console.log('req arrived')
+res.json({
+    msg : data
+})
+
+})
+server.listen(port,url,()=>{
+    console.log(`server is up at ${url} on port:${port}`)
+})
+
+function randomMeow(){
+    let resLen = Math.floor(Math.random() * 10) + 5;
+    let msg = '🐱 meow ';
+    for(let i = 0 ; i<resLen ; i++)
+    msg = msg + 'meow '
+    return msg;
+}
+
